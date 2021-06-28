34a9725e03473c6733778744887e1a592f0a2643
walterswoodward <walter.sekope.woodward@gmail.com>
2021-06-28 11:15:09 -0700

    Adds 404.pug

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ app.use(shopRoutes);
 
 // Handle 404
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {docTitle: 'Page Not Found'});
 })
 
 app.listen(3000);
