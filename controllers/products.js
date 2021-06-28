d760ef53e07b6da949f79b9716fa64f587b56d1d
walterswoodward <walter.sekope.woodward@gmail.com>
2021-06-28 14:16:41 -0700

    Revise code to store data in a json file and store/access using NodeJS's fs module

--- controllers/products.js
+++ controllers/products.js
@@ -17,13 +17,12 @@ exports.postAddProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-    const products = Product.fetchAll();
-    res.render('shop', {
+    Product.fetchAll((products) => res.render('shop', {
         prods: products, 
         pageTitle: 'Shop',
         path: '/',
         hasProducts: products.length > 0,
         activeShop: true,
         bootstrapCSS: true
-    });
+    }));
 }
--- models/product.js
+++ models/product.js
@@ -1,4 +1,19 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../util/path');
+
+const p = path.join(
+    rootDir,
+    'data',
+    'products.json'
+);
+
+const getProductsFromFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) return cb([]);
+        cb(JSON.parse(fileContent));
+    })
+}
 
 module.exports = class Product {
     constructor(title) {
@@ -6,10 +21,13 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
+        getProductsFromFile((products) => {
+            products.push(this); // note that 'this' refers to the containing class only b/c it is inside of an arrow function
+            fs.writeFile(p, JSON.stringify(products), (err) => console.log(err));
+        });
     }
 
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+        getProductsFromFile(cb);
     }
 }
