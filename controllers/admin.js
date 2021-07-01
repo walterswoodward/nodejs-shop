45ecd609f35a49959143e69390eae9aa0b0f21fa
walterswoodward <walter.sekope.woodward@gmail.com>
2021-07-01 16:29:29 -0700

    Revises methods in Admin Controller file to use sequelize ORM methods

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,27 +14,31 @@ exports.getEditProduct = (req, res) => {
         return res.redirect('/');
     }
     const prodId = req.params.productId;
-    Product.findById(prodId, product => {
-        if (!product) {
-            // TODO: Replace this with error message
-            return res.redirect('/');
-        }
-        res.render('admin/edit-product', {
-            pageTitle: 'Edit Product',
-            path: '/admin/edit-product',
-            editing: editMode,
-            product: product
-        });
-    })
-}
+    Product.findByPk(prodId)
+        .then(product => {
+            if (!product) {
+                // TODO: Replace this with error message
+                return res.redirect('/');
+            }
+            res.render('admin/edit-product', {
+                pageTitle: 'Edit Product',
+                path: '/admin/edit-product',
+                editing: editMode,
+                product: product
+            });
+        })
+        .catch(err => console.log(err));
+    }
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll((products) => res.render('admin/products', {
-        prods: products,
-        pageTitle: 'Admin Products',
-        path: '/admin/products',
-        hasProducts: products.length > 0
-    }));
+    Product.findAll().then(products => 
+        res.render('admin/products', {
+            prods: products,
+            pageTitle: 'Admin Products',
+            path: '/admin/products',
+            hasProducts: products.length > 0
+        })
+    ).catch(err => console.log(err));
 }
 
 exports.postAddProduct = (req, res, next) => {
@@ -45,28 +49,43 @@ exports.postAddProduct = (req, res, next) => {
         imageUrl: imageUrl,
         description: description
     })
-        .then(result => console.log('Created Product!'))
+        .then(result => {
+            console.log('Created Product!');
+            res.redirect('/admin/products');
+        })
         .catch(err => console.log(err));
 }
 
 exports.postEditProduct = (req, res, next) => {
     const prodId = req.body.productId;
     const { title, imageUrl, price, description } = req.body;
-    const updatedProduct = new Product(
-        prodId,
-        title,
-        imageUrl,
-        price,
-        description
-    );
-    // TODO: Move redirect inside of updateProduct.save as callback
-    updatedProduct.save();
-    res.redirect('/admin/products');
+    Product.findByPk(prodId)
+        .then(product => {
+            product.title = title;
+            product.imageUrl = imageUrl;
+            product.price = price;
+            product.description = description;
+            // You may be tempted to chain a then and catch block to this method but in order to avoid the same messiness of nested callbacks
+            // instead we can return this call to the save method (which returns a Promise) and handle to the results in the next then block
+            return product.save();
+        })
+        .then(result => {
+            console.log("Product Updated!");
+            res.redirect('/admin/products');
+        })
+        .catch(err => console.log(err));
 }
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
     // TODO: Move redirect inside of Product.deleteById as callback
-    Product.deleteById(prodId);
+    Product.findByPk(prodId)
+    .then(product => {
+        return product.destroy();
+    })
+    .then(result => {
+        console.log('Product Destroyed!');
+    })
+    .catch(err => console.log(err));
     res.redirect('/admin/products');
 }
