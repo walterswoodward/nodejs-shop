8ac19b4bcbef91b10987aecc5cdc87779f933034
walterswoodward <walter.sekope.woodward@gmail.com>
2021-07-01 18:21:22 -0700

    Establishes relationships between Product and User models and hardcodes user with id 1 before server goes live

--- app.js
+++ app.js
@@ -12,13 +12,22 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const { get404 } = require('./controllers/error');
 const sequelize = require('./util/database');
-
+const Product = require('./models/product');
+const User = require('./models/user');
 
 // convert express requests to json
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
+
+// Note that this anonymous function was "registered" as middleware for all incoming request, 
+// but is not called until a request is made to the server (started previously with `npm start`)
+app.use((req, res, next) => {
+    User.findByPk(1) 
+        .then(user => {req.user = user; next();})
+        .catch(err => console.log(err));
+});
 
 // express.static(root, [options]) -- grants access to these dependencies
 // Note (from docs) that you can designate multiple static assets directories: https://expressjs.com/en/starter/static-files.html
@@ -34,8 +43,26 @@ app.use(shopRoutes);
 // Handle 404
 app.use(get404);
 
-sequelize.sync()
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
+sequelize
+    // .sync({ force: true }) // Tables recreated each time
+    .sync()
     .then(result => {
+        return User.findByPk(1);
+    })
+    .then(user => {
+        if (!user) {
+            User.create({
+                name: 'Walter',
+                email: 'walter@nodejs.com'
+            });
+        }
+        return user;
+    })
+    .then(user => {
+        // console.log(user);
         app.listen(3000);
     })
     .catch(err => console.log(err));
