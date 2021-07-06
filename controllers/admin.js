66dbd868b2e7b543de5c0305dc546f1a1d8f48b6
walterswoodward <walter.sekope.woodward@gmail.com>
2021-07-06 12:24:15 -0700

    Reorders methods in admin routes and controllers by view page name instead of http method

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,11 +1,38 @@
 const Product = require('../models/product');
 
+exports.getProducts = (req, res, next) => {
+    Product
+        .fetchAll()
+        .then(products => 
+        res.render('admin/products', {
+            prods: products,
+            pageTitle: 'Admin Products',
+            path: '/admin/products',
+            hasProducts: products.length > 0
+        })
+    ).catch(err => console.log(err));
+}
+
 exports.getAddProduct = (req, res) => {
     res.render('admin/edit-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
         editing: false // this isn't really necessary
     });
+}
+
+exports.postAddProduct = (req, res, next) => {
+    const { title, imageUrl, price, description } = req.body;
+
+    // This syntax works only b/c of the associations established between products and users in app.js
+    const product = new Product(title, price, description, imageUrl)
+    product
+        .save()
+        .then(result => {
+            console.log('Created Product!');
+            res.redirect('/admin/products');
+        })
+        .catch(err => console.log(err));
 }
 
 exports.getEditProduct = (req, res) => {
@@ -26,33 +53,6 @@ exports.getEditProduct = (req, res) => {
                 editing: editMode,
                 product: product
             });
-        })
-        .catch(err => console.log(err));
-    }
-
-exports.getProducts = (req, res, next) => {
-    Product
-        .fetchAll()
-        .then(products => 
-        res.render('admin/products', {
-            prods: products,
-            pageTitle: 'Admin Products',
-            path: '/admin/products',
-            hasProducts: products.length > 0
-        })
-    ).catch(err => console.log(err));
-}
-
-exports.postAddProduct = (req, res, next) => {
-    const { title, imageUrl, price, description } = req.body;
-
-    // This syntax works only b/c of the associations established between products and users in app.js
-    const product = new Product(title, price, description, imageUrl)
-    product
-        .save()
-        .then(result => {
-            console.log('Created Product!');
-            res.redirect('/admin/products');
         })
         .catch(err => console.log(err));
 }
--- routes/admin.js
+++ routes/admin.js
@@ -3,20 +3,22 @@ const { getAddProduct, postAddProduct, getProducts, getEditProduct, postEditProd
 
 const router = express.Router();
 
+// /admin/products => GET
+router.get('/products', getProducts);
+
+// ADD PRODUCT
 // /admin/add-product => GET
 router.get('/add-product', getAddProduct);
 
 // /admin/add-product => POST
 router.post('/add-product', postAddProduct);
 
+// EDIT PRODUCT
 // /admin/edit-product => GET
 router.get('/edit-product/:productId', getEditProduct);
 
 // /admin/edit-product => POST
 router.post('/edit-product', postEditProduct);
-
-// /admin/products => GET
-router.get('/products', getProducts);
 
 // /admin/delete-product => POST
 // router.post('/delete-product', postDeleteProduct);
