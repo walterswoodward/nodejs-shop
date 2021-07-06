7a7fe0843bf56269f7ed7a70846e538846c4d7ca
walterswoodward <walter.sekope.woodward@gmail.com>
2021-07-06 12:18:56 -0700

    Reorders admin routes by path rather than http method

--- routes/admin.js
+++ routes/admin.js
@@ -6,17 +6,17 @@ const router = express.Router();
 // /admin/add-product => GET
 router.get('/add-product', getAddProduct);
 
-// /admin/edit-product => GET
-// router.get('/edit-product/:productId', getEditProduct);
-
-// /admin/products => GET
-router.get('/products', getProducts);
-
 // /admin/add-product => POST
 router.post('/add-product', postAddProduct);
 
+// /admin/edit-product => GET
+router.get('/edit-product/:productId', getEditProduct);
+
 // /admin/edit-product => POST
-// router.post('/edit-product', postEditProduct);
+router.post('/edit-product', postEditProduct);
+
+// /admin/products => GET
+router.get('/products', getProducts);
 
 // /admin/delete-product => POST
 // router.post('/delete-product', postDeleteProduct);
